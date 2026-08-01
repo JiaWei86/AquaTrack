81c1c99642d9983ff6e7427272e9cce65e9bef66
Yiteng1202 <yiteng1202@gmail.com>
2026-08-01 09:36:51 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,38 +17,28 @@ Route::get('/', function () {
 // Authentication
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Protected routes
 Route::middleware('auth')->group(function () {
-
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
     Route::resource('complaints', ComplaintController::class);
-
     Route::resource('water-sources', WaterSourceController::class);
 
     // User Management
     Route::resource('users', UserController::class)->except(['update']);
-
-    Route::patch(
-        'users/{user}/status',
-        [UserController::class, 'update']
-    )->name('users.update.status');
+    Route::patch('users/{user}/status', [UserController::class, 'update'])
+        ->name('users.update.status');
 
     // API
     Route::get('api/inspectors', [UserController::class, 'inspectorInfo'])
         ->name('api.inspectors');
-
     Route::get('api/users', [UserController::class, 'userInfo'])
         ->name('api.users');
 });
 
 // Other Resources
 Route::resource('quality-readings', QualityReadingController::class);
-
 Route::resource('alerts', AlertController::class);
